f25e6800891a01c51bba3e2903d3ab25a93c2aa3
Nawar T <nawarturk11@gmail.com>
2023-10-22 10:58:05 -0400

    Make Minimax the Default Temporarily

--- ai_wargame.py
+++ ai_wargame.py
@@ -222,7 +222,8 @@ class Options:
     min_depth : int | None = 2
     max_time : float | None = 5.0
     game_type : GameType = GameType.AttackerVsDefender
-    alpha_beta : bool = True
+#put it back to True
+    alpha_beta : bool = False 
     max_turns : int | None = 100
     randomize_moves : bool = True
     broker : str | None = None
